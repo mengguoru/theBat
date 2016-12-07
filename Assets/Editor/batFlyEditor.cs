302ac888df714c14ae106b5451ab517b797ce100
mengguoru <mengguoru0@gmail.com>
2016-12-07 17:12:35 +0800

    camera to bat circle

---
+++ Assets/Editor/batFlyEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(batFly))]
+public class batFlyEditor : Editor {
+
+    private void OnSceneGUI()
+    {
+        batFly t = target as batFly;
+        if (null == t)
+            return;
+
+        Handles.color = Color.blue;
+        Handles.DrawLine(t.camPos, t.targetPos);
+        Handles.DrawWireDisc(t.targetPos, (t.camPos - t.targetPos), t.radius);
+    }
+}
---
+++ Assets/Scripts/batFly.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class batFly : MonoBehaviour {
+    public Vector3 camPos;
+    public GameObject target;
+    public Vector3 targetPos;
+    public Vector3 offset;
+    public float radius;
+
+	// Use this for initialization
+	void Start () {
+        camPos = this.transform.position;
+        offset = target.transform.position - camPos;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        camPos = this.transform.position;
+        targetPos = camPos + offset;
+    }
+}

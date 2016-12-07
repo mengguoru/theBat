86375f3ace579df92436e1c5195a1f2cd4f81c5d
mengguoru <mengguoru0@gmail.com>
2016-12-07 16:33:49 +0800

    dd

---
+++ Assets/Shaders/colorByDistance.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class colorByDistance : MonoBehaviour {
+    public Material mat = null;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

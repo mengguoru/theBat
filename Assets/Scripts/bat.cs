9c0b7f33d4f00c68a1f6a0337ac8c937962cd46e
mengguoru <mengguoru0@gmail.com>
2016-12-10 17:57:28 +0800

    before add shader

--- Assets/Scripts/bat.cs
+++ Assets/Scripts/bat.cs
@@ -10,9 +10,16 @@ public class bat : MonoBehaviour {
 
     //simulate flight force
     public float flightForce;
+
+    //for rotation
+    public GameObject rotatePart;
+    public Quaternion originRotation;
+    public float angleDegree;
+
 	// Use this for initialization
 	void Start () {
         theBat = this.gameObject;
+        originRotation = transform.rotation;
 	}
 	
 	// Update is called once per frame
@@ -22,8 +29,19 @@ public class bat : MonoBehaviour {
         Vector3 flightOffset = new Vector3(hori * speed * Time.deltaTime, veri *speed * Time.deltaTime, flightForce * Time.deltaTime);
         //transform.Translate(hori, veri, flightForce*Time.deltaTime);
         transform.position += flightOffset;
-        Debug.Log(hori + ":"+veri);
+        //Debug.Log(hori + ":"+veri);
         //theBat.transform.Rotate(hori* rotateSpeed, veri*rotateSpeed, 0);
         //theBat.transform.Rotate(hori * rotateSpeed * Time.deltaTime, veri * rotateSpeed * Time.deltaTime, 0);
+        //if(0 == hori && 0 == veri)
+        //{
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, originRotation, rotateSpeed*Time.time);
+        //}
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            rotatePart.gameObject.transform.Rotate(0, angleDegree, 0);
+        }else if(Input.GetKeyDown(KeyCode.Q))
+        {
+            rotatePart.gameObject.transform.Rotate(0, -angleDegree, 0);
+        }
     }
 }
